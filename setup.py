4b07f9f57f924900899a7e8331b7f754fc9f312f
unknown <18829354152@163.com>
2018-09-14 19:15:02 +0800

    upgrade version number

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gradientdecent',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/chZzZzZz/gradientdecent.git',
+    license='MIT',
+    author='chZzZzZz',
+    author_email='chzzz714@gmail.com',
+    description='gradientdecent implementation',
+    long_description=open("Readme.md").read(),
+    long_description_content_type='text/markdown'
+)
